6db44264511b565c7db0a2bd087c36a366ae2d6b
Cay Zhang <13341339+Cay-Zhang@users.noreply.github.com>
2024-11-30 17:11:51 -0600

    Provide more context

--- pages/content-ui/src/index.tsx
+++ pages/content-ui/src/index.tsx
@@ -23,7 +23,7 @@ function showAcronymPopover({
   structuredContainer.setAttribute('data-popover-id', String(popoverId));
 
   acronym ??= structuredContainer.textContent!;
-  context ??= structuredContainer.closest(':not(span):not(mark)')?.textContent || acronym;
+  context ??= structuredContainer.closest(':not(span):not(mark):not(code)')?.textContent || acronym;
 
   const mark = structuredContainer.querySelector('mark');
   if (!mark) return;
